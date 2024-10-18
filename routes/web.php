92c660ab6a50b83d13ce8d0823002c20eb41a1ae
johnmarkbanisilon <zionsploit@gmail.com>
2024-10-18 17:45:17 +0800

    php: add admin routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,37 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Login');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Admin/index');
+    });
+
+    Route::get('/announcement', function () {
+        return Inertia::render('Admin/Announcement');
+    });
+
+    Route::prefix('/socio-cultural')->group(function () {
+        Route::get('/events', function () {
+            return Inertia::render('Admin/SocioCultural/Events');
+        });
+
+        Route::get('/officials', function () {
+            return Inertia::render('Admin/SocioCultural/Official');
+        });
+    });
+
+    Route::prefix('/e-sports')->group(function () {
+        Route::get('/events', function() {
+            return Inertia::render('Admin/ESports/Events');
+        });
+
+        Route::get('/officials', function() {
+            return Inertia::render('Admin/ESports/Official');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
